69db8d2f5f2b02936a6eed246764f5b351684876
kpazfagundes <kpazfagundes@gmail.com>
2018-12-27 23:28:29 -0200

    Updates

---
+++ precache-manifest.82ebcba30885dcfe2febbb48eca6526d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d6386bee464989660a3",
+    "url": "/irrigation-project/static/js/runtime~main.1d6386be.js"
+  },
+  {
+    "revision": "ad945f2083e4df13413c",
+    "url": "/irrigation-project/static/js/main.ad945f20.chunk.js"
+  },
+  {
+    "revision": "c34e56f51b444a6bce8e",
+    "url": "/irrigation-project/static/js/1.c34e56f5.chunk.js"
+  },
+  {
+    "revision": "ad945f2083e4df13413c",
+    "url": "/irrigation-project/static/css/main.ef12284e.chunk.css"
+  },
+  {
+    "revision": "c34e56f51b444a6bce8e",
+    "url": "/irrigation-project/static/css/1.5ce9e3bd.chunk.css"
+  },
+  {
+    "revision": "d56367064663711ad4db3461b5ea0f67",
+    "url": "/irrigation-project/index.html"
+  }
+];
